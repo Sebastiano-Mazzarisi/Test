31b713ad33a7d4a7d01fd39dc7ae31d2d93fbe6e
Sebastiano-Mazzarisi <s.mazzarisi@mazzarisi.it>
2025-06-15 08:45:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', function(event) {
+  // Per ora, questo service worker non fa nulla di speciale,
+  // ma la sua presenza è fondamentale per rendere l'app installabile.
+});
